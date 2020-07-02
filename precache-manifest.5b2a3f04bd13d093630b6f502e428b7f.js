207794ef8bab85785c900c2a4c3b10d1e82de7c6
nidarg <gradin_g@yahoo.com>
2020-07-03 00:53:10 +0300

    Updates

---
+++ precache-manifest.5b2a3f04bd13d093630b6f502e428b7f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373c2b4e899dd0a33ad15dbba775dde4",
+    "url": "/monsters-react-basis/index.html"
+  },
+  {
+    "revision": "85d58796635f1592b49d",
+    "url": "/monsters-react-basis/static/css/main.bce27624.chunk.css"
+  },
+  {
+    "revision": "3c6206427d404668659c",
+    "url": "/monsters-react-basis/static/js/2.c7b44d30.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-react-basis/static/js/2.c7b44d30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d58796635f1592b49d",
+    "url": "/monsters-react-basis/static/js/main.292bc826.chunk.js"
+  },
+  {
+    "revision": "288db3fac9c54553f8c9",
+    "url": "/monsters-react-basis/static/js/runtime-main.2c3d867c.js"
+  }
+]);
